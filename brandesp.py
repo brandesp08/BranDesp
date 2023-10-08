43953682a1707b65f259516f07e9e25f374dae29
brandesp08 <147084748+brandesp08@users.noreply.github.com>
2023-10-08 16:45:26 +0200

    Create brandesp.py
    code

---
+++ brandesp.py
@@ -0,0 +1,72 @@
+#Phyton lernen
+
+#valuables
+
+frist_name = "Bro"
+last_name = "Despi"
+full_name = frist_name +" "+ last_name
+print("Hello " + full_name)
+
+
+age = 15
+age += 1
+print("Your age is "+str(age))
+#print(type(age))
+
+
+
+height = 250.5
+print ("Your high is: "+str(height) + "cm")
+#print(type(height))
+
+human = False
+print("Are you a human "+str(human))
+#print(type(human))
+
+
+#name= "Bro"
+#age= 21
+#attractive = True
+
+name , age, attractive, = "Bro", 21, True
+
+print(name)
+print(age)
+print(attractive)
+
+
+Spongebob = Patrick = Sandy = Squid = 18
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squid)
+
+
+
+name = "BranDesp"
+
+print(len(name))
+print(name.find("r"))
+print(name.capitalize())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("n"))
+print(name.replace("r","a"))
+print(name*4)
+
+
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+
+x = float(x)
+y = float(y)
+z = float(z)
+
+print(x)
+print(y)
+print(z*3)
+
+input("What is your name?:")
